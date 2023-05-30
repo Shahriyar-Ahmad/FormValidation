959ebd3a6de581d0304d4376a7ba54c2bbf30061
Shahriyar <growbro405@gmail.com>
2023-05-30 15:35:18 +0500

    My first commit after some update

---
+++ fv.js
@@ -0,0 +1,68 @@
+var ne = document.getElementById("name-erore");
+var pe = document.getElementById("phone-erore");
+var ee = document.getElementById("email-erore");
+var me = document.getElementById("messege-erore");
+var se = document.getElementById("submit-erore");
+function nameValidate() {
+    var name = document.getElementById("fullname").value;
+    if (name.lenght == 0) {
+        ne.innerHTML = "Name is required";
+        return false;
+    }
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+        ne.innerHTML = "Full Name is required.";
+        return false;
+    }
+    ne.innerHTML = "<i class='fa-sharp fa-solid fa-circle-check'></i>";
+    return true;
+}
+function phoneValidate() {
+    var phone = document.getElementById("phone-no").value;
+    if (phone.lenght == 0) {
+        pe.innerHTML = "Phone No is required";
+        return false;
+    }
+    // if (phone.lenght !== 11) {
+    //     pe.innerHTML = "Write Your No Accuratly.";
+    //     return false;
+    // }
+    if (!phone.match(/^[0-9]{11}$/)) {
+        pe.innerHTML = "Only Eleven Digits";
+        return false;
+    }
+    pe.innerHTML = "<i class='fa-sharp fa-solid fa-circle-check'></i>";
+    return true;
+}
+function emailValidate() {
+    var email = document.getElementById("email-no").value;
+    if (email.length == 0) {
+        ee.innerHTML = "Email is Required.";
+        return false;
+    }
+    if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        ee.innerHTML = "Email Invalid";
+        return false;
+    }
+    ee.innerHTML = "<i class='fa-sharp fa-solid fa-circle-check'></i>";
+    return true;
+}
+function messeageValidate() {
+    var messge = document.getElementById("messeage").value;
+
+    var required = 30;
+    var left = required - messge.length;
+    if (left < 0) {
+        me.innerHTML = "Charactor Above the Limit." ;
+        return false;
+    }
+    me.innerHTML = "<i class='fa-sharp fa-solid fa-circle-check'></i>";
+    return true;
+}
+function formValidate() {
+    if (!nameValidate() || !phoneValidate() || !messeageValidate() || !emailValidate) {
+        se.innerHTML = "Plz fix the erorr."
+        se.style.display = "block";
+        setTimeout(function () { se.style.display = "none"; }, 3000);
+        return false;
+    }
+}
